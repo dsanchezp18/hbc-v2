c7728c2d19f849a9e921bd69958ef88aefe88451
Daniel Sanchez <dsanchezp998@gmail.com>
2022-06-20 13:12:53 -0500

    Finished literature review, start methodology. Also create the tabulations.R script.

---
+++ scripts/tabulations.R
@@ -0,0 +1,10 @@
+## ---- chunk-tabulations
+
+# ==================================== LAPOP ECUADOR TABULATIONS  ========================================================
+
+# Daniel Sánchez
+# USFQ Economics Undergraduate Thesis
+# June 2022
+# Script for the calculations of survey-weighted tabulations of the LAPOP study
+# Dependent on the data_manipulation.R script- either run it first to run this script as "standalone" or use in a knitr context
+
